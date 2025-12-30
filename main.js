79c967f081b40480739dfe473d02c8f33d7a655e
Awab Abdelbagi <awaababdelbagi@gmail.com>
2025-12-30 15:45:00 +0300

    Refactor showData function and add date handling

--- main.js
+++ main.js
@@ -3,18 +3,81 @@ const cityInput = document.getElementById("cites"),
     sectionOfDate = document.getElementById("dateInfoData"),
     table = document.getElementById("table");
 
+dateInput.value = getToday();
+
 function showData() {
     const selcetCity = cityInput.value,
         selectDate = dateInput.value;
-    console.log(selcetCity);
-    axios.get(`https://api.aladhan.com/v1/timingsByCity/29-07-2025?city=Riyadh&country=SA&method=4&shafaq=general&tune=5%2C3%2C5%2C7%2C9%2C-1%2C0%2C8%2C-6&school=0&midnightMode=0&timezonestring=Asia%2FRiyadh&latitudeAdjustmentMethod=2&calendarMethod=UAQ`)
+
+    let country = selcetCity.split(" ")[1];
+    let city = selcetCity.split(" ")[0].split("/")[1];
+    let timezon = selcetCity.split(" ")[0].split("/")[0];
+    let date = selectDate.split("-").reverse().join("-");
+
+    axios.get(`https://api.aladhan.com/v1/timingsByCity/${date}?city=${city}&country=${country}&method=4&shafaq=general&tune=5%2C3%2C5%2C7%2C9%2C-1%2C0%2C8%2C-6&school=0&midnightMode=0&timezonestring=${timezon}%2F${city}&latitudeAdjustmentMethod=2&calendarMethod=UAQ`)
         .then(function (response) {
             const data = response.data.data;
+
+            sectionOfDate.innerHTML = `
+            <h3>
+                <span>${data.date.hijri.weekday.ar}</span>
+                <span>${data.date.hijri.day}</span>
+                <span>${data.date.hijri.month.ar}</span>
+                <span>${data.date.hijri.year}</span>
+            </h3>
+            <div class="text-dark-emphasis">${date}</div>
+            `;
+
+            table.innerHTML = `
+                <thead class="table-dark">
+                    <tr>
+                        <th scope="col">الصلاة</th>
+                        <th scope="col">الوقت</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">الفجر</th>
+                        <td>${data.timings.Fajr}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">الشروق</th>
+                        <td>${data.timings.Sunrise}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">الظهر</th>
+                        <td>${data.timings.Dhuhr}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">العصر</th>
+                        <td>${data.timings.Asr}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">المغرب</th>
+                        <td>${data.timings.Maghrib}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">العشاء</th>
+                        <td>${data.timings.Isha}</td>
+                    </tr>
+                </tbody>
+            `;
+            document.getElementById("note").innerHTML = data.meta.method.name;
         })
         .catch(function (error) {
             alert(`API Requst Erorr: ${error}`);
         });
 }
-// cityInput.addEventListener("change", showData);
-// dateInput.addEventListener("change", showData);
+
+function getToday() {
+    const d = new Date();
+    const day   = String(d.getDate()).padStart(2, "0");
+    const month = String(d.getMonth() + 1).padStart(2, "0");
+    const year  = d.getFullYear();
+    return `${year}-${month}-${day}`;
+}
+
+
+cityInput.addEventListener("change", showData);
+dateInput.addEventListener("change", showData);
 showData();
